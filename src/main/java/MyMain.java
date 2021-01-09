eb261a1a83d3f983a11cbd0e50526c3ba2d9d089
22cburrows <22cburrows@athenian.org>
2021-01-09 22:17:23 +0000

    testing

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,20 +1,36 @@
+import java.util.Arrays;
+import java.util.Random;
 public class MyMain {
 
     // Shuffles an array, using the perfect shuffle algorithm
     public static int[] perfectShuffle(int[] arr) { 
-        int[] arr2 = new int[arr.length];
-        for (int i = 0; i < arr.length; i++){
-            for (int j = arr.length/2; j < arr.length; j++){
-                arr2[i] = arr[i];
-                arr2[i+1] = arr[j];
-            }
+        //int[] arr2 = new int[arr.length];
+        int[] firstHalf = new int[arr.length/2];
+        int[] secondHalf = new int[arr.length/2];
+
+        for (int i = 0; i < arr.length/2; i++){
+            firstHalf[i] = arr[i];
+        }
+        for (int i = arr.length/2; i < arr.length/2; i++){
+            secondHalf[i] = arr[i];
+        }
+        for (int i = 0; i < arr.length/2; i++){
+            arr[i*2] = firstHalf[i];
+            arr[(i*2) + 1] = secondHalf[i];
         } 
-        return arr2;
+        return arr;
     }
 
     // Shuffles an array, using the selection shuffle algorithm
     public static int[] selectionShuffle(int[] arr) { 
-        return null;
+        Random rand = new Random();
+        for (int i = 0; i < arr.length; i++){
+            int randNum = rand.nextInt(arr.length);
+            int num = arr[randNum];
+            arr[randNum] = arr[i];
+            arr[i] = num;
+        }
+        return arr;
     }
 
 
