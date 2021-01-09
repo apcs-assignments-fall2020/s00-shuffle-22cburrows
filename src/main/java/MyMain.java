536a2467155fbaca17fe2b2354ab82cebbd8e92a
22cburrows <22cburrows@athenian.org>
2021-01-09 21:27:37 +0000

    perfect shuffle

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,7 +2,14 @@ public class MyMain {
 
     // Shuffles an array, using the perfect shuffle algorithm
     public static int[] perfectShuffle(int[] arr) { 
-        return null;
+        int[] arr2 = new int[arr.length];
+        for (int i = 0; i < arr.length; i++){
+            for (int j = arr.length/2; j < arr.length; j++){
+                arr2[i] = arr[i];
+                arr2[i+1] = arr[j];
+            }
+        } 
+        return arr2;
     }
 
     // Shuffles an array, using the selection shuffle algorithm
